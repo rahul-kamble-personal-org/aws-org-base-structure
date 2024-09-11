b46856eacb1ed0aafdd1e0b88a6c814fe3711778
Rahul Kamble <50138555+RAHULKAMBLE781998@users.noreply.github.com>
2024-09-11 12:15:47 +0530

    nat

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,8 @@
-
 # VPC resource
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
-
   tags = merge(
     local.default_tags,
     {
@@ -16,7 +14,6 @@ resource "aws_vpc" "main" {
 # Internet Gateway
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
-
   tags = merge(
     local.default_tags,
     {
@@ -30,7 +27,6 @@ resource "aws_subnet" "public_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "eu-central-1a"
-
   tags = merge(
     local.default_tags,
     {
@@ -43,7 +39,6 @@ resource "aws_subnet" "public_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "eu-central-1b"
-
   tags = merge(
     local.default_tags,
     {
@@ -56,7 +51,6 @@ resource "aws_subnet" "private_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "eu-central-1a"
-
   tags = merge(
     local.default_tags,
     {
@@ -69,7 +63,6 @@ resource "aws_subnet" "private_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.4.0/24"
   availability_zone = "eu-central-1b"
-
   tags = merge(
     local.default_tags,
     {
@@ -78,15 +71,35 @@ resource "aws_subnet" "private_2" {
   )
 }
 
-# Route Table for Public Subnets
+# NAT Gateway and Elastic IP
+resource "aws_eip" "nat" {
+  domain = "vpc"
+  tags = merge(
+    local.default_tags,
+    {
+      Name = "NAT Gateway EIP"
+    }
+  )
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_1.id
+  tags = merge(
+    local.default_tags,
+    {
+      Name = "Main NAT Gateway"
+    }
+  )
+}
+
+# Route Tables
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id
   }
-
   tags = merge(
     local.default_tags,
     {
@@ -95,7 +108,21 @@ resource "aws_route_table" "public" {
   )
 }
 
-# Route Table Associations for Public Subnets
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+  tags = merge(
+    local.default_tags,
+    {
+      Name = "Private Route Table"
+    }
+  )
+}
+
+# Route Table Associations
 resource "aws_route_table_association" "public_1" {
   subnet_id      = aws_subnet.public_1.id
   route_table_id = aws_route_table.public.id
@@ -106,19 +133,6 @@ resource "aws_route_table_association" "public_2" {
   route_table_id = aws_route_table.public.id
 }
 
-# Route Table for Private Subnets
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-
-  tags = merge(
-    local.default_tags,
-    {
-      Name = "Private Route Table"
-    }
-  )
-}
-
-# Route Table Associations for Private Subnets
 resource "aws_route_table_association" "private_1" {
   subnet_id      = aws_subnet.private_1.id
   route_table_id = aws_route_table.private.id
@@ -134,7 +148,6 @@ resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
   vpc_id      = aws_vpc.main.id
-
   ingress {
     description = "SSH from anywhere"
     from_port   = 22
@@ -142,14 +155,12 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   tags = merge(
     local.default_tags,
     {
