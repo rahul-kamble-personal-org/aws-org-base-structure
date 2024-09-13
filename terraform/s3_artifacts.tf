ecba3ad0c1adc0661171868424783cec2eca2111
Rahul Kamble <50138555+RAHULKAMBLE781998@users.noreply.github.com>
2024-09-13 10:41:04 +0530

    art

--- terraform/s3_artifacts.tf
+++ terraform/s3_artifacts.tf
@@ -1,21 +1,21 @@
-# Create a basic S3 bucket
-resource "aws_s3_bucket" "artifact-bucket" {
-  bucket = "rahul-kamble-personal-org-lambda-artifacts"
+# # Create a basic S3 bucket
+# resource "aws_s3_bucket" "artifact-bucket" {
+#   bucket = "rahul-kamble-personal-org-lambda-artifacts"
 
-  tags = {
-    Name        = "lambda-artifacts-1"
-    Environment = "Development"
-  }
-}
+#   tags = {
+#     Name        = "lambda-artifacts-1"
+#     Environment = "Development"
+#   }
+# }
 
-# Output the bucket name
-output "s3_bucket_name" {
-  description = "The name of the S3 bucket"
-  value       = aws_s3_bucket.artifact-bucket.id
-}
+# # Output the bucket name
+# output "s3_bucket_name" {
+#   description = "The name of the S3 bucket"
+#   value       = aws_s3_bucket.artifact-bucket.id
+# }
 
-# Output the bucket ARN
-output "s3_bucket_arn" {
-  description = "The ARN of the S3 bucket"
-  value       = aws_s3_bucket.artifact-bucket.arn
-}
+# # Output the bucket ARN
+# output "s3_bucket_arn" {
+#   description = "The ARN of the S3 bucket"
+#   value       = aws_s3_bucket.artifact-bucket.arn
+# }
